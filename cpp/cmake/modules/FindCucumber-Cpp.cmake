88deddf92c393f1df9c3567fbf0e80bb6db3934a
AndreasAugustin <andy.augustin@t-online.de>
2016-04-10 18:31:08 +0200

    moved subprojects into own github repo

--- cpp/cmake/modules/FindCucumber-Cpp.cmake
+++
@@ -1,7 +0,0 @@
-if(NOT Cucumber-Cpp_FOUND)
-    set(Cucumber-Cpp_ROOT_DIR ${CMAKE_MODULE_PATH}/../../vendor/cucumber-cpp)
-    add_subdirectory(${Cucumber-Cpp_ROOT_DIR} cucumbercpp)
-    set(Cucumber-Cpp_INCLUDE_DIRS ${Cucumber-Cpp_ROOT_DIR}/include)
-    set(Cucumber-Cpp_LIBRARY Cucumber-Cpp)
-    set(Cucumber-Cpp_FOUND True)
-endif()
